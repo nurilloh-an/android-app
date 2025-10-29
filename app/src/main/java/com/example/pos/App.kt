04e45361d171b3233a1fd30be069a70727226e4a
Nurilloh <nurillox.anvarjonov@bellissimo.uz>
2025-10-29 16:03:55 +0500

    new changes

--- app/src/main/java/com/example/pos/App.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pos
-
-import android.app.Application
-import com.example.pos.data.AppDatabase
-
-class PosApp : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
